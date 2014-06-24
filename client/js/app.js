bd85ed90a036e1a2d9eb95cbf3c62f417e1c0315
Nathan Moon <nmoon@hirevue.com>
2014-06-24 16:30:45 -0600

    Stub in a form post

--- client/js/app.js
+++ client/js/app.js
@@ -2,6 +2,15 @@
 
 var app = angular.module('superRap', []);
 
-app.controller('HeroCtrl', function($scope) {
+app.controller('HeroCtrl', function($scope, $http) {
     $scope.title = 'Heroes';
+
+    $scope.createHero = function(hero) {
+        console.log('creating hero ' + hero);
+        $http.post('/api/heroes', hero).success(function(data, status, headers, config) {
+            console.log('success');
+        }).error(function(data, status, headers, config) { 
+            console.log('failed'); 
+        });
+    };
 });
--- server/routes/api.js
+++ server/routes/api.js
@@ -7,5 +7,11 @@ router.get('/heroes', function(req, res, next) {
     res.json([{id: 1, name: 'Batman'}, {id: 2, name: 'Wonder Woman'}]);
 });
 
+router.post('/heroes', function(req, res, next) {
+    console.log('post to heroes');
+    res.writeHead(204);
+    res.end();
+});
+
 
 module.exports = router;
