e86f9312a92cd72ef38a8aa1b0dd3c07c04bbe9e
Nathan Moon <nmoon@hirevue.com>
2014-06-25 14:41:21 -0600

    Better pattern for returning responses

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,18 +4,25 @@ var router = express.Router();
 
 
 router.get('/heroes', function(req, res, next) {
-    res.json([{id: 1, name: 'Batman'}, {id: 2, name: 'Wonder Woman'}]);
+    console.log(req.cookies.get('user_id'));
+    res.send(200, [{id: 1, name: 'Batman'}, {id: 2, name: 'Wonder Woman'}]);
 });
 
 router.post('/heroes', function(req, res, next) {
+    res.cookies.set('user_id', req.body.name);
     console.log('post to heroes');
     console.log(req.body);
-    res.writeHead(204);
-    res.end();
+    res.send(204);
 });
 
 router.get('/login', function(req, res, next) {
-    res.cookies.set('user_id', 'blah');
+    var user = {id: 1}; // lookup user
+    if (!user) {
+        res.send(400, 'user not found');
+    } else {
+        res.cookies.set('user_id', user.id);
+        res.send(200, user)
+    }
 });
 
 
