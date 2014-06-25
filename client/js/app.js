0f19141e93702d21ff3e4c611491189f3b39bd0b
Nathan Moon <nmoon@hirevue.com>
2014-06-25 11:48:23 -0600

    Add angular routing into the mix

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,22 @@
 // angular app config will go here
 
-var app = angular.module('superRap', []);
+var app = angular.module('superRap', [
+        'ngRoute']);
+
+app.config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.when(
+                '/heroes', {
+                    templateUrl: 'partials/heroes-list.html',
+                    controller: 'HeroCtrl'
+            }).when(
+                '/hero-form', {
+                    templateUrl: 'partials/hero-form.html',
+                    controller: 'HeroCtrl'
+            }).otherwise({
+                redirectTo: '/heroes'
+            });
+        }]);
 
 app.controller('HeroCtrl', function($scope, $http) {
     $scope.title = 'Heroes';
