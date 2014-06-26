4de90d3a46926239019b62afd5bfc27d4be9e29b
Nathan Moon <nmoon@hirevue.com>
2014-06-26 10:58:46 -0600

    add bootstrap css, get alter-ego stuff working better

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,7 @@
 
 var app = angular.module('superRap', [
-        'ngRoute']);
+        'ngRoute',
+        'ngCookies']);
 
 // url routes
 app.config(['$routeProvider',
@@ -9,6 +10,10 @@ app.config(['$routeProvider',
                 '/people', {
                     templateUrl: 'partials/people-list.html',
                     controller: 'PeopleListCtrl'
+            }).when(
+                '/person/:personId', {
+                    templateUrl: 'partials/person-details.html',
+                    controller: 'PersonDetailsCtrl'
             }).when(
                 '/person-form', {
                     templateUrl: 'partials/person-form.html',
@@ -20,10 +25,22 @@ app.config(['$routeProvider',
 
 // controllers (until we decide to move them out somewhere else)
 
-app.controller('AppCtrl', function($scope) {
+app.controller('AppCtrl', function($scope, $cookies) {
     $scope.$on('alter-ego-assumed', function(event, ego) {
-        $scope.currentEgo = ego;
+        console.log($cookies);
+        if (ego) {
+            $scope.currentEgo = ego;
+        } else if ($cookies.ego_id) {
+            $scope.currentEgo = {id: $cookies.ego_id, name: $cookies.ego_name};
+        }
     });
+    $scope.$on('alter-ego-dropped', function(event) {
+        console.log($cookies);
+        console.log('logging out');
+        $scope.currentEgo = null;
+    });
+
+    $scope.$emit('alter-ego-assumed');
 });
 
 app.controller('PersonFormCtrl', function($scope, $http, $location) {
@@ -37,6 +54,23 @@ app.controller('PersonFormCtrl', function($scope, $http, $location) {
             console.log('failed'); 
         });
     };
+});
+
+app.controller('PersonDetailsCtrl', function($scope, $http, $routeParams) {
+    $scope.id = $routeParams.personId;
+    load($scope.id);
+
+    $scope.$watch('id', function(newId, oldId) {
+        if (newId != oldId) {
+            load(newId);
+        }
+    });
+
+    function load(id) {
+        $http.get('/api/people/' + id).success(function(data, status, headers, config) {
+            $scope.person = data;
+        }).error(errorHandler);
+    }
 });
 
 function errorHandler(data, status, headers, config) {
@@ -64,4 +98,14 @@ app.controller('AlterEgoCtrl', function($scope, $http) {
             $scope.$emit('alter-ego-assumed', data);
         }).error(errorHandler);
     };
+
+    $scope.becomeNobody = function() {
+        console.log('becoming nobody');
+        $http.get('/api/alterego').success(function(data, status, headers, config) {
+            console.log('emitting logout message');
+            $scope.$emit('alter-ego-dropped');
+        }).error(errorHandler);
+        console.log('argh');
+    };
+
 });
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 var router = express.Router();
 
+router.use(function(req, res, next) {
+    res.set('Cache-Control', 'max-age=0, no-cache, no-store');
+    res.set('Pragma', 'no-cache');
+    next();
+});
 
 router.get('/people', function(req, res, next) {
     console.log(req.cookies.get('ego_id'));
@@ -18,17 +23,26 @@ router.post('/people', function(req, res, next) {
 router.get('/people/:id', function(req, res, next) {
     console.log(req.cookies.get('ego_id'));
     console.log(req.params.id);
-    res.send(200, {id: 1, name: 'Batman', skills: ['rich', 'dark']});
+    res.send(200, {id: 1, name: 'Batman', skills: ['rich', 'dark'], friends: ['his butler', 'that one girl']});
 });
 
 router.get('/alterego', function(req, res, next) {
     console.log(req.query.id);
-    var ego = {id: 1, name: 'Invisible Man'}; // lookup person
-    if (!ego) {
-        res.send(400, 'ego not found');
+    if (req.query.id) {
+        var ego = {id: 1, name: 'Invisible Man'}; // lookup person
+        if (!ego) {
+            res.send(400, 'ego not found');
+        } else {
+            console.log('ego: ' + ego.id + ', ' + ego.name);
+            res.cookies.set('ego_id', ego.id, {httpOnly: false});
+            res.cookies.set('ego_name', ego.name, {httpOnly: false});
+            res.send(200, ego)
+        }
     } else {
-        res.cookies.set('ego_id', ego.id);
-        res.send(200, ego)
+        // logout
+        console.log('logging out');
+        res.cookies.set('ego_id').set('ego_name');
+        res.send(200, {})
     }
 });
 
